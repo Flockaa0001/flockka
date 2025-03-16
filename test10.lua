e8cf4033c9357d46e6f9258e3103f2390c7d7d65
Flockaa0001 <121258400+Flockaa0001@users.noreply.github.com>
2025-03-16 17:57:07 -0500

    Update test10.lua

--- test10.lua
+++ test10.lua
@@ -25,11 +25,11 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
         ['Bezier'] = (true), --[[ -- ]]
         ['Sticky'] = (true), --[[ Single Target ]]
 
-        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.79), ['Air'] = (0.079) },
+        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.053), ['Air'] = (0.083) },
 
-        ['Prediction'] = { ['Enabled'] = (false), ['Ground'] = (0.12), ['Air'] = (0.12) },
+        ['Prediction'] = { ['Enabled'] = (true), ['Ground'] = (0.079), ['Air'] = (0.079) },
                                                             --[[ Basic, Advanced ]]
-        ['Point'] = ('Default'), ['Scale'] = (0.2), ['Type'] = ('Basic'), --[[ "Nearest Part", "Nearest Point", "Default" ]]
+        ['Point'] = ('Nearest Point'), ['Scale'] = (0.2), ['Type'] = ('Basic'), --[[ "Nearest Part", "Nearest Point", "Default" ]]
         ['Parts'] = {'Head'}, --[[ https://create.roblox.com/docs/reference/engine/enums/BodyPartR15 ]]
 
         ['Easing'] = ('Linear')
