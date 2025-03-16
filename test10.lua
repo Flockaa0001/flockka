c4683fe5213630fc33023fab5a084913eb689942
Flockaa0001 <121258400+Flockaa0001@users.noreply.github.com>
2025-03-16 17:24:34 -0500

    Update test10.lua

--- test10.lua
+++ test10.lua
@@ -27,7 +27,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
 
         ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.066), ['Air'] = (0.088) },
 
-        ['Prediction'] = { ['Enabled'] = (true), ['Ground'] = (0.12), ['Air'] = (0.12) },
+        ['Prediction'] = { ['Enabled'] = (false), ['Ground'] = (0.12), ['Air'] = (0.12) },
                                                             --[[ Basic, Advanced ]]
         ['Point'] = ('Default'), ['Scale'] = (0.2), ['Type'] = ('Basic'), --[[ "Nearest Part", "Nearest Point", "Default" ]]
         ['Parts'] = {'Head'}, --[[ https://create.roblox.com/docs/reference/engine/enums/BodyPartR15 ]]
