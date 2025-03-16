bcec3c4980958f5606ec9147b2918bc5e5efb354
Flockaa0001 <121258400+Flockaa0001@users.noreply.github.com>
2025-03-16 17:54:50 -0500

    Update test10.lua

--- test10.lua
+++ test10.lua
@@ -25,7 +25,7 @@ shared["F​F​l​a​g​A​X​C​o​m​b​i​n​e​G​e​t​O​
         ['Bezier'] = (true), --[[ -- ]]
         ['Sticky'] = (true), --[[ Single Target ]]
 
-        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.066), ['Air'] = (0.088) },
+        ['Smoothing'] = { ['Enabled'] = (true), ['Ground'] = (0.79), ['Air'] = (0.079) },
 
         ['Prediction'] = { ['Enabled'] = (false), ['Ground'] = (0.12), ['Air'] = (0.12) },
                                                             --[[ Basic, Advanced ]]
